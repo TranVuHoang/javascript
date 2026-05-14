1b1df0ec58b4c24f0382d47977c4f57c63772523
TranVuHoang <tranvuhoangjr@gmail.com>
2026-05-14 11:39:45 +0700

    #05 Cấu trúc code trong JavaScript

---
+++ unicode/module2-javascript-basic/5-struct/code-struct/index.js
@@ -0,0 +1,10 @@
+// Đây là chương trình demo
+console.log("Câu lệnh 1");
+[1, 2].forEach(() => console.log("ABC"));
+console.log("Câu lệnh 2");
+console.log(1 + 2 + 3);
+
+/**
+ * Kết thúc chương trình demo
+ * Tôi là Hoàng
+ */
