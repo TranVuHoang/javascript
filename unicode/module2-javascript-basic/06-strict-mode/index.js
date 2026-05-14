f58dd5851b4149db01e51d4e21936574e768ea5f
TranVuHoang <tranvuhoangjr@gmail.com>
2026-05-14 17:47:29 +0700

    #06 strict mode

---
+++ unicode/module2-javascript-basic/06-strict-mode/index.js
@@ -0,0 +1,21 @@
+"use strict";
+const message = "Học lập trình không khó";
+console.log(message);
+
+const helloWorld = () => {
+  let message = "Học lập trình không khó";
+  console.log(message);
+};
+helloWorld();
+
+const helloWorld2 = () => {
+  let message2 = "Học lập trình không khó";
+  console.log(message2);
+};
+helloWorld2();
+
+const user = {
+  name: "Hoàng Trần",
+};
+delete user.name;
+console.log(user);
