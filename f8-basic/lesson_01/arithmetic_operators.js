36313fa25efd9d8392ef0bad86b7c157f29d18d5
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-04 09:48:07 +0700

    #9. Toán tử số học trong JavaScript | Các loại toán tử số học JS

---
+++ f8-basic/lesson_01/arithmetic_operators.js
@@ -0,0 +1,37 @@
+/**
+ * Toán tử số học trong JS
+ * +  --> Cộng
+ * -  --> Trừ
+ * *  --> Nhân
+ * /  --> Chia
+ * **  --> Luỹ thừa
+ * %  --> Chia lấy dư
+ * ++  --> Tăng lên 1 giá trị
+ * --  --> Giảm đi 1 giá trị
+ */
+let a = 3;
+let b = 2;
+let c = a + b;
+
+console.log(">>> check c=", c);
+c = a - b;
+
+console.log(">>> check c=", c);
+
+c = a * b;
+console.log(">>> check c=", c);
+
+c = a / b;
+console.log(">>> check c=", c);
+
+c = a ** b;
+console.log(">>> check c=", c);
+
+c = a % b;
+console.log(">>> check c=", c);
+
+c = a++;
+console.log(">>> check c=", c);
+
+c = a--;
+console.log(">>> check c=", c);
