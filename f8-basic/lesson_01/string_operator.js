9ae67913d426d7c2bc637f8f8ec940bac78cc134
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-05 06:20:44 +0700

    #12. Toán tử chuỗi (String Operator) trong JavaScript | khi nào sử dụng toán tử chỗi

---
+++ f8-basic/lesson_01/string_operator.js
@@ -0,0 +1,9 @@
+/**
+ * Toán tử chuỗi - string operator
+ */
+let firstName = "Hoàng";
+let lastName = "Trần";
+
+console.log(firstName, lastName); // Hoàng Trần
+console.log(firstName + " " + lastName); // HoàngTrần
+console.log(firstName + " Trần");
