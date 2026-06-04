25c4307779bac0aaf4f739a8c2a068f3d85e936a
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-04 09:39:40 +0700

    #8.Làm quen với toán tử trong JavaScript | Các loại toán tử JS

---
+++ f8-basic/lesson_01/operator.js
@@ -0,0 +1,22 @@
+/**
+ * Giới thiệu về toán tử trong JS
+ * 1. Toán tử số học: Arithmetic
+ * 2. Toán tử gán - . Assignment
+ * 3. Toán tử so sánh - Comparison
+ * 4. Toán tử logic - Logical
+ */
+
+let a = 1 + 2;
+console.log(a);
+
+const fullname = "Trần Vũ Hoàng";
+a = 1;
+let b = 2;
+
+if (a > b) {
+  console.log("Đúng");
+  alert(`${a} > ${b}`);
+} else {
+  console.log("Sai");
+  alert(`${a} < ${b}`);
+}
---
+++ f8-basic/operator.md
@@ -0,0 +1,9 @@
+```js
+/**
+ * Giới thiệu về toán tử trong JS
+ * 1. Toán tử số học: Arithmetic
+ * 2. Toán tử gán - . Assignment
+ * 3. Toán tử so sánh - Comparison
+ * 4. Toán tử logic - Logical
+ * /
+```
