e85e3ccd21f710387150320e2bfa9bb9308efe7a
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-05 06:53:18 +0700

    #13. Toán tử so sánh trong Javascript (phần 1)

---
+++ f8-basic/lesson_01/ comparison_operator.js
@@ -0,0 +1,26 @@
+/**
+ * Toán tử so sánh - comparison operator
+ *
+ * Toán tử
+ * ==     -> Bằng
+ * !=     -> Khác
+ * >      -> Lớn hơn
+ * <      -> Nhỏ hơn
+ * >=     -> Lớn hơn hoặc bằng
+ * <=     -> Nhỏ hơn hoặc bằng
+ */
+let a = 1;
+let b = 2;
+let c = "3";
+
+if (a == b) {
+  console.log("Điều kiện đúng.");
+} else {
+  console.log("Điều kiện sai.");
+}
+
+if (a < c) {
+  console.log("Điều kiện đúng.");
+} else {
+  ("Điều kiện sai.");
+}
