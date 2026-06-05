dad65232141e2be2df22949e4ae326ed0bfdf4c0
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-05 10:00:46 +0700

    #20. Chuỗi trong JavaScript | JavaScript String

---
+++ f8-basic/lesson_01/string_type.js
@@ -0,0 +1,19 @@
+/**
+ * CHUỖI TRONG JAVASCRIPT
+ *
+ * 1. Tạo chuỗi
+ * - Các cách tạo chuỗi
+ * - Nên dùng các nào? Lý do?
+ * - Kiểm tra data type
+ *
+ * 2. Một số case sử dung backslash (\)
+ * 3. Xem độ dài chuỗi
+ * 4. Chú ý độ dài khi viết code
+ * 5. Template string - Template literal ES6 (``)
+ */
+let fullname = "Hoàng IT 'đẹp trai'";
+console.log(">>> fullname:", fullname);
+console.log(">>> độ dài chuỗi:", fullname.length);
+
+let fullname2 = `Trang "xinh yêu"`;
+console.log(">>> fullname2:", fullname2);
