94daf934abcf95a6ab25aa1f228bce1e1fbbe648
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-05 07:08:52 +0700

    #15. Câu lệnh điều kiện If - Else trong JavaScript

---
+++ f8-basic/lesson_01/if_else_condition.js
@@ -0,0 +1,19 @@
+/**
+ * if-else: Câu lệnh điều kiện
+ */
+/**
+ * Các giá trị khi convert sang boolean sẽ thành: false
+ * 0
+ * false
+ * "" or ''
+ * undefined
+ * NaN
+ * null
+ */
+let isSuccess = 1 > 2;
+
+if ('') {
+  console.log("Điều kiện đúng.");
+} else {
+  console.log("Điều kiện sai.");
+}
