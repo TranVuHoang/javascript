de7df6286359e50c92c9b7ba3835c3e538b3944f
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-05 08:47:11 +0700

    #18. Toán tử so sánh trong JavaScript (phần 2)

---
+++ f8-basic/lesson_01/ comparison_operator_f2.js
@@ -0,0 +1,10 @@
+/** Toán tử so sánh - F2 */
+
+/**
+ * === : so sánh (=) cả về giá trị(value) và data type(kiểu dữ liệu)
+ * !==
+ */
+let a = 1;
+let b = "1";
+console.log(a == b); // true
+console.log(a === b); // false
