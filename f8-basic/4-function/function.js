c4f92bf04fdd2484ae486c52806619e642e5836d
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-12 08:38:22 +0700

    #25. Hàm trong JS

---
+++ f8-basic/4-function/function.js
@@ -0,0 +1,27 @@
+/**
+ * Hàm(function) trong JS
+ *
+ * 1. Hàm?
+ * - Một khối mã
+ * - Làm 1 việc cụ thể
+ *
+ * 2. Lọai hàm
+ * - Built-in
+ * - Tự định nghĩa
+ *
+ * 3. Tính chất
+ * - Không thực thi khi định nghĩa
+ * - Sẽ thực thi khi được gọi
+ * - Có thể nhận tham số
+ * - Có thể trả về 1 giá trị
+ *
+ * 4. Tạo hàm đầu tiên
+ */
+
+// Định nghĩa(tạo) hàm
+function showDiaLog() {
+  console.log("Hi, xin chào các bạn.");
+}
+
+// gọi hàm
+showDiaLog();
