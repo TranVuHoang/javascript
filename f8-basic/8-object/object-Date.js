2c97d007d9bb863b37023a7c88a89b4603c5de99
TranVuHoang <tranvuhoangjr@gmail.com>
2026-06-13 08:34:00 +0700

    #34. Date object trong JS

---
+++ f8-basic/8-object/object-Date.js
@@ -0,0 +1,17 @@
+// object Date
+var date = new Date();
+console.log(">>> date:", date);
+
+// lấy ra năm hiện tại
+console.log("Year:", date.getFullYear());
+// tháng
+console.log("Month:", date.getMonth() + 1);
+//ngày
+console.log("Day:", date.getDate());
+
+// giờ
+console.log("Giờ:", date.getHours());
+// phút
+console.log("Phút:", date.getMinutes());
+// giây
+console.log("Giây:", date.getSeconds());
