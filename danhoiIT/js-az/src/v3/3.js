2dee0ce6b48ad33de8c584a012bec9629f478f3b
TranVuHoang <tranvuhoangjr@gmail.com>
2026-08-02 15:54:38 +0700

    #7 các kiểu dữ liệu javascript

---
+++ danhoiIT/js-az/src/v3/3.js
@@ -0,0 +1,26 @@
+console.log("Hello world from javascript");
+
+/**
+ * Kiểu dữ liệu number
+ */
+let c = 5;
+let d = 5.9;
+
+/**
+ * Kiểu dữ liệu string
+ */
+console.log(`Hello ' " world.`);
+
+/**
+ * Kiểu dữ liệu boolean
+ */
+console.log(c == d);
+
+/**
+ * Kiểm tra kiểu dữ liệu của biến
+ */
+console.log(typeof c); // number
+
+/** Hằng số */
+const pi = 3.14;
+console.log("PI= ", pi);
